4ab6eeeb089c3848bafbb43202d86fdf79b7cbc0
Ayofe Farouq <ayofef@gmail.com>
2020-02-29 23:36:57 +0000

    testing

---
+++ precache-manifest.33aeaff1f292c102d88bac522d6ee9b6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf634081c3e1d28095b272c81313bd7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daab5d3ef05230a254bc",
+    "url": "/static/css/main.eedbe15f.chunk.css"
+  },
+  {
+    "revision": "b0d7463f7144146bd747",
+    "url": "/static/js/2.1b3a4cf4.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.1b3a4cf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daab5d3ef05230a254bc",
+    "url": "/static/js/main.0e7378e6.chunk.js"
+  },
+  {
+    "revision": "34d7cb50c0c4ba0a79b1",
+    "url": "/static/js/runtime-main.f3e806a6.js"
+  },
+  {
+    "revision": "598b2dfda0803b331f9501821aa147bd",
+    "url": "/static/media/abigail.598b2dfd.jpg"
+  },
+  {
+    "revision": "18da384b3c581c145d6901e0c5044d84",
+    "url": "/static/media/abigailbg.18da384b.jpg"
+  },
+  {
+    "revision": "6070307ce435c74361a13f16fa7e0c85",
+    "url": "/static/media/competition.6070307c.jpg"
+  },
+  {
+    "revision": "3a1df6c1e0644b8b1a12219346c3f013",
+    "url": "/static/media/curvy.3a1df6c1.png"
+  },
+  {
+    "revision": "f456e9048fe35b5524acc39bcfd9df9f",
+    "url": "/static/media/hermes.f456e904.jpg"
+  },
+  {
+    "revision": "3d679e19c28d57a4e06103de7861ebb7",
+    "url": "/static/media/jc-gellidon-XmYSlYrupL8-unsplash.3d679e19.jpg"
+  },
+  {
+    "revision": "f4721a6943103706b42740185bcb62d5",
+    "url": "/static/media/jeffrey.f4721a69.jpg"
+  },
+  {
+    "revision": "80a4aec6e5116902ec71641fa06cedc8",
+    "url": "/static/media/kawasaki.80a4aec6.jpg"
+  },
+  {
+    "revision": "29ed876f7ac45db1e1e23cac2cee326a",
+    "url": "/static/media/kick.29ed876f.jpg"
+  },
+  {
+    "revision": "b5260355ec1142ceda0efccf56986837",
+    "url": "/static/media/markus.b5260355.jpg"
+  },
+  {
+    "revision": "400202f539113d6bb8c6e1aaddff60ed",
+    "url": "/static/media/soccer.400202f5.png"
+  },
+  {
+    "revision": "e4d7e7075128c864fa1618accfce7073",
+    "url": "/static/media/sprite.e4d7e707.svg"
+  }
+]);
